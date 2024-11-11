b8255e8789ab65a5c30f42f7428ee6b9019c94e7
RelkzTfrog <sr.relkz01@gmail.com>
2024-11-11 17:49:58 -0300

    iniciar2

---
+++ ProjetoFinal/Q2/src/functions.h
@@ -0,0 +1,15 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+typedef struct
+{
+    long comparacoes;
+    long trocas;
+} Metrics;
+
+void bubbleOptimized(int arr[], int n, Metrics *metrica);
+void bubbleSort(int v[], int n, Metrics *metrica);
+void bucketSort(int V[], int N, Metrics *metrica);
+void bucketOptimized(int V[], int N, Metrics *metrica);
+void insertionSort(int arr[], int n, Metrics *metrica);
+#endif
